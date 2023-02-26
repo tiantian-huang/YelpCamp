14cc95fa775cf04af59d18763f4c14970419a268
tiantian-huang <huang.tianti@northeastern.edu>
2023-02-25 23:40:39 -0800

    yelpCamp project

--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,9 @@ const cities = require('./cities');
 const {places, descriptors}  = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
-
+// mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
